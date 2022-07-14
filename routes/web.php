f89dd609320b43cf1134fb5a641082b31b1565b9
vuducnguyentb <vuducnguyentb@gmail.com>
2022-07-14 22:15:24 +0700

    migration

--- routes/web.php
+++ routes/web.php
@@ -18,21 +18,25 @@ Route::get('/', function () {
 //    $user = DB::select('select * from users where id = ?',[1]);
 //    $result = DB::select('select * from users where id = ? and name = ?',[1,'Garth Morar']);
 //    $result = DB::table('users')->select()->get();
-    DB::transaction(function () {
+//    DB::transaction(function () {
 //       DB::table('users')->where('id',6)->update(['email'=>'none']);
 //       DB::table('users')->where('id',5)->update(['email'=>'none@@gmail.com']);
 
         // try catch block is not necessary as well as DB::rollBack();
-        try {
-            DB::table('users')->delete();
-            $result = DB::table('users')->where('id', 6)->update(['email' => 'none']);
-            if (!$result) {
-                throw new \Exception;
-            }
-        } catch (\Exception $e) {
-            DB::rollBack();
-        }
-    }, 5);
+//        try {
+//            DB::table('users')->delete();
+//            $result = DB::table('users')->where('id', 6)->update(['email' => 'none']);
+//            if (!$result) {
+//                throw new \Exception;
+//            }
+//        } catch (\Exception $e) {
+//            DB::rollBack();
+//        }
+//    }, 5);
 
+    $users = DB::table('users')->get();
+    $comments = DB::table('comments')->get();
+
+    
     return view('welcome');
 });
