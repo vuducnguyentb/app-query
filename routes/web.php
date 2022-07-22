1df0ca4e3360cca3db2445ca86addcc428984b96
Vũ Đức Nguyên <nguyenvd@tcv.vn>
2022-07-22 13:47:36 +0700

    Full text search

--- routes/web.php
+++ routes/web.php
@@ -4,33 +4,33 @@ use App\Models\Comment;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
+
 //Route::get('/', function () {
-////    $result = DB::table('rooms')
-////        ->whereBetween('room_size', [1, 3]) //whereNotBetween
-////        ->get();
-////    $result = DB::table('rooms')
-////        ->whereNotIn('id', [1,2, 3]) //whereIn
-////        ->get();
-////    //whereNull()
-////
-////    $result = DB::table('users')
-////        ->whereExists(function($query){
-////            $query->select('id')
-////                ->from('reservations')
-////                ->whereRaw('reservations.user_id = user_id')
-////                ->where('check_in','=','2022-07-21')
-////                ->limit(1);
-////        })
-////        ->get();
+//    $result = DB::table('users')
+////    ->whereJsonContains('meta->skills','Laravel')
+//    ->where('meta->settings->site_language','en')
+//    ->get();
 //
 //    return view('welcome');
 //});
-
 Route::get('/', function () {
-    $result = DB::table('users')
-//    ->whereJsonContains('meta->skills','Laravel')
-    ->where('meta->settings->site_language','en')
-    ->get();
 
+//    $result = DB::statement('ALTER TABLE comments ADD FULLTEXT fulltext_index(content)'); //innoDB - MySQL >= 5.6
+    $result = DB::table('comments')
+        ->whereRaw("MATCH(content) AGAINST(? IN BOOLEAN MODE)",
+            ['repellendus']
+        )
+        ->get();
+//        $result = DB::table('comments')
+//        ->where('content','like','%repellendus%')
+//        ->get();
+    $result = DB::table('comments')
+        ->whereRaw("MATCH(content) AGAINST(? IN BOOLEAN MODE)",
+            ['+repellendus -est']
+        )
+        ->get();
+
+    dump($result);
     return view('welcome');
 });
+
