7609a43cb94640929b91b83ab6131435f35e881d
Vũ Đức Nguyên <nguyenvd@tcv.vn>
2022-07-22 14:02:21 +0700

    Sql Raw

--- routes/web.php
+++ routes/web.php
@@ -6,31 +6,38 @@ use Illuminate\Support\Facades\Route;
 
 
 //Route::get('/', function () {
-//    $result = DB::table('users')
-////    ->whereJsonContains('meta->skills','Laravel')
-//    ->where('meta->settings->site_language','en')
-//    ->get();
 //
+////    $result = DB::statement('ALTER TABLE comments ADD FULLTEXT fulltext_index(content)'); //innoDB - MySQL >= 5.6
+//    $result = DB::table('comments')
+//        ->whereRaw("MATCH(content) AGAINST(? IN BOOLEAN MODE)",
+//            ['repellendus']
+//        )
+//        ->get();
+////        $result = DB::table('comments')
+////        ->where('content','like','%repellendus%')
+////        ->get();
+//    $result = DB::table('comments')
+//        ->whereRaw("MATCH(content) AGAINST(? IN BOOLEAN MODE)",
+//            ['+repellendus -est']
+//        )
+//        ->get();
+//
+//    dump($result);
 //    return view('welcome');
 //});
-Route::get('/', function () {
 
-//    $result = DB::statement('ALTER TABLE comments ADD FULLTEXT fulltext_index(content)'); //innoDB - MySQL >= 5.6
+Route::get('/', function () {
     $result = DB::table('comments')
-        ->whereRaw("MATCH(content) AGAINST(? IN BOOLEAN MODE)",
-            ['repellendus']
-        )
-        ->get();
-//        $result = DB::table('comments')
-//        ->where('content','like','%repellendus%')
-//        ->get();
-    $result = DB::table('comments')
-        ->whereRaw("MATCH(content) AGAINST(? IN BOOLEAN MODE)",
-            ['+repellendus -est']
-        )
-        ->get();
+    ->selectRaw('count(user_id) as number_of_comments, users.name')
+    ->join('users','users.id','=','comments.user_id')
+    ->groupBy('user_id')
+    ->get();
+    //whereRaw / orWhereRaw
+    //havingRaw / orHavingRaw
+    //orderByRaw
+    //groupByRaw
+    //orderByRaw
 
     dump($result);
     return view('welcome');
 });
-
