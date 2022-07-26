3b1e2e205c5e9b71fccc0026dd4c2b7f76a64107
Vũ Đức Nguyên <nguyenvd@tcv.vn>
2022-07-26 11:14:47 +0700

    UPDATE

--- routes/web.php
+++ routes/web.php
@@ -5,37 +5,27 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 //Route::get('/', function () {
-//    $users = DB::table('users')
-//    ->select('name');
+//    DB::table('rooms')->insert([
+//        ['room_number' => 1, 'room_size' => 1, 'price' => 1, 'description' => 'new description 1'],
+//    ]);
 //
-//    $result = DB::table('cities')
-//    ->select('name')
-//    ->union($users)
-//    ->get();
+//    $id = DB::table('rooms')->insertGetId(
+//        ['room_number' => 3, 'room_size' => 3, 'price' => 3, 'description' => 'new description 3'],
+//    );
 //
-//    $comments = DB::table('comments')
-//    ->select('rating as rating_or_room_id','id',DB::raw
-//    ('"comments" as type_of_activity'))
-//    ->where('user_id',2);
-//    $result = DB::table('reservations')
-//    ->select('room_id as rating_or_room_id','id',DB::raw
-//    ('"reservations" as type_of_activity'))
-//    ->union($comments)
-//    ->where('user_id',2)
-//    ->get();
-//
-//  return view('welcome');
+//    return view('welcome');
 //});
 
+
 Route::get('/', function () {
-    DB::table('rooms')->insert([
-        ['room_number' => 1, 'room_size' => 1, 'price' => 1, 'description' => 'new description 1'],
-    ]);
+    $affected = DB::table('rooms')
+    ->where('id',1)
+    ->update(['price'=>222]);
 
-    $id = DB::table('rooms')->insertGetId(
-        ['room_number' => 3, 'room_size' => 3, 'price' => 3, 'description' => 'new description 3'],
-    );
+    $affected = DB::table('users')
+    ->where('id',1)
+    ->update(['meta->settings->site_language'=>'es']);
 
-    dump($id);
+    dump($affected);
     return view('welcome');
 });
