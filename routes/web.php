c5e828bd8fdf147bc4852abb3d6ad99fd7aa407a
Vũ Đức Nguyên <nguyenvd@tcv.vn>
2022-07-26 14:46:50 +0700

    UPDATE operations

--- routes/web.php
+++ routes/web.php
@@ -5,28 +5,29 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 //Route::get('/', function () {
-//    $result = Comment::all()->toArray();
-//    $result = Comment::all()->toJson();
-//    $comments = Comment::all();
-//    $result = $comments->reject(function($comment){
-//    return $comment->rating <3;
-//    });
-//    $result = $comments->diff($result);
+//    $comment = new Comment();
+//    $comment->user_id = 1;
+//    $comment->rating = 5;
+//    $comment->content = 'comment content';
+//    $result=$comment->save();
+//
+//    $result = Comment::create([
+//        'user_id'=>1,
+//        'rating'=>5,
+//        'content'=>'comment content'
+//    ]);
+//
 //    return view('welcome');
 //});
 Route::get('/', function () {
-    $comment = new Comment();
+    $comment = Comment::find(1);
     $comment->user_id = 1;
     $comment->rating = 5;
     $comment->content = 'comment content';
     $result=$comment->save();
 
-    $result = Comment::create([
-    'user_id'=>1,
-    'rating'=>5,
-    'content'=>'comment content'
-    ]);
+    $result = Comment::where('price','<',200)
+    ->update(['price'=>250]);
 
-    dump($result);
     return view('welcome');
 });
