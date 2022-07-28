278db6e0300cf8eb5a1d41bcf099b3d8956cba27
Vũ Đức Nguyên <nguyenvd@tcv.vn>
2022-07-28 14:09:14 +0700

    Querying and counting database relationships (sql select)

--- routes/web.php
+++ routes/web.php
@@ -8,13 +8,22 @@ use App\Models\User;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
-
 Route::get('/', function () {
-//    $result = User::find(3);
-//    dump($result->image);
-//        $result = Room::find(10);
-//        dump($result->comments);
-    $result = User::find(1);
-    dump($result->likedImages);
+//    $result = User::find(1)->comments()
+//    ->where('rating','>',3)
+//    ->orWhere('rating','>',2)
+//    ->get();
+
+//    $result = User::find(1)->comments()
+//        ->where(function ($query) {
+//            return $query->where('rating', '>', 3)
+//                ->orWhere('rating', '<', 2);
+//        })
+//        ->get();
+
+//    $result = User::has('comments','>=',2)->get();
+//    $result = Comment::has('user.address')->get();
+        $result = User::withCount('comments')->get();
+    dump($result);
     return view('welcome');
 });
